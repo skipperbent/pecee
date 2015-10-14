38efef19540176db72e25f1688dbd22300dd169e
Simon Sessingø <simon.sessingoe@gmail.com>
2015-10-14 20:28:41 +0200

    [BUGFIX] Fixed ModelLanguage so it now uses getIdentifier instead of missing getName() method.

--- src/Model/ModelLanguage.php
+++ src/Model/ModelLanguage.php
@@ -24,7 +24,7 @@ class ModelLanguage extends \Pecee\Model\Model {
 
     protected static function getContext() {
         $route = RouterBase::getInstance()->getLoadedRoute();
-        if($route->getName()) {
+        if($route->getIdentifier()) {
             return $route->getIdentifier();
         }
         return '';
