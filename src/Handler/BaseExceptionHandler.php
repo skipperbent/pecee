1b6f860cd1840df0ab85b2d83eb8eb7f7b09d00e
Simon Sessingø <simon.sessingoe@gmail.com>
2015-11-01 08:27:46 +0100

    [FEATURE] Added BaseExceptionHandler class which inherits from Base class.

--- src/Handler/BaseExceptionHandler.php
+++ src/Handler/BaseExceptionHandler.php
@@ -1,10 +1,11 @@
 <?php
 namespace Pecee\Handler;
 
+use Pecee\Base;
 use Pecee\Http\Request;
 use Pecee\SimpleRouter\RouterEntry;
 
-abstract class ExceptionHandler {
+abstract class BaseExceptionHandler extends Base {
 
 	abstract public function handleError( Request $request, RouterEntry $router = null, \Exception $error);
 
