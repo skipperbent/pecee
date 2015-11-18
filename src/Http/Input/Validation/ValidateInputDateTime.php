7395709910ecbd4faa8c56527caf400dabbd3e8a
Simon Sessingø <simon.sessingoe@gmail.com>
2015-11-18 05:23:38 +0100

    [BUGFIX] Fixed wrong reference in ValidateInputDate and ValidateInputDateTime validator classes.

--- src/Http/Input/Validation/ValidateInputDateTime.php
+++ src/Http/Input/Validation/ValidateInputDateTime.php
@@ -1,12 +1,14 @@
 <?php
 namespace Pecee\Http\Input\Validation;
 
+use Pecee\Date;
+
 class ValidateInputDateTime extends ValidateInput {
 
 	public function validate() {
-		return (\Pecee\Date::IsValid($this->value));
+		return (empty($this->value) || Date::isDate($this->value));
 	}
-	
+
 	public function getErrorMessage() {
 		return lang('%s is not a valid date time', $this->name);
 	}
