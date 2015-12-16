50533826fbd9565b1194b0b2a54f33a0c5d10d27
Simon Sessingø <simon.sessingoe@gmail.com>
2015-12-16 02:33:48 +0100

    [BUGFIX] Added increment for user-data model.

--- src/Model/User/UserData.php
+++ src/Model/User/UserData.php
@@ -1,5 +1,6 @@
 <?php
 namespace Pecee\Model\User;
+
 use Pecee\DB\DBTable;
 use Pecee\Model\Model;
 
@@ -7,7 +8,7 @@ class UserData extends Model {
 	public function __construct($userId = null, $key = null, $value = null) {
 
         $table = new DBTable();
-		$table->column('id')->bigint()->primary();
+		$table->column('id')->bigint()->primary()->increment();
         $table->column('user_id')->bigint()->index();
         $table->column('key')->string(255)->index();
         $table->column('value')->longtext()->nullable();
@@ -18,6 +19,7 @@ class UserData extends Model {
         $this->key = $key;
         $this->value = $value;
 	}
+
 	public function save() {
 		if(self::scalar('SELECT `key` FROM {table} WHERE `key` = %s AND `user_id` = %s', $this->key, $this->userId)) {
 			parent::update();
@@ -29,6 +31,7 @@ class UserData extends Model {
 	public static function removeAll($userId) {
 		self::nonQuery('DELETE FROM {table} WHERE `user_id` = %s', array($userId));
 	}
+
 	public static function getByUserId($userId) {
 		return self::fetchAll('SELECT * FROM {table} WHERE `user_id` = %s', array($userId));
 	}
