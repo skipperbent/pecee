c1a21f9d49cb2cb7c81dcc35f1ce06fbfd202200
Simon Sessingø <simon.sessingoe@gmail.com>
2016-01-23 23:21:34 +0100

    Added removeclass method.

--- src/UI/Menu/Menu.php
+++ src/UI/Menu/Menu.php
@@ -29,7 +29,7 @@ class Menu {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Returns first item.
 	 * @return \Pecee\UI\Menu\MenuItems|null
@@ -42,7 +42,7 @@ class Menu {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Returns last item.
 	 * @return \Pecee\UI\Menu\MenuItems|null
@@ -53,7 +53,7 @@ class Menu {
 		}
 		return null;
 	}
-	
+
 	/**
 	 * Check if the item-index exists.
 	 * @param int $index
@@ -62,11 +62,11 @@ class Menu {
 	public function hasItem($index) {
 		return isset($this->items[$index]);
 	}
-	
+
 	public function hasItems() {
 		return (count($this->items) > 0);
 	}
-	
+
 	/**
 	 * Add form content
 	 * @param \Pecee\UI\Html\Html $element
@@ -74,7 +74,7 @@ class Menu {
 	public function addContent(Html $element) {
 		$this->content[] = $element;
 	}
-	
+
 	/**
 	 * Add form content
 	 * @param \Pecee\UI\Menu\Menu $element
@@ -82,7 +82,7 @@ class Menu {
 	public function addMenu(Menu $element) {
 		$this->content[] = $element;
 	}
-	
+
 	/**
 	 * Get form content, if any
 	 * @return \Pecee\UI\Html\Html|null
@@ -90,7 +90,7 @@ class Menu {
 	public function getContent() {
 		return $this->content;
 	}
-	
+
 	/**
 	 * Add new item
 	 *
@@ -104,7 +104,7 @@ class Menu {
 		$this->items[] = $item;
 		return $item;
 	}
-	
+
 	/**
 	 * Add new item
 	 *
@@ -115,8 +115,8 @@ class Menu {
 		$this->items[] = $item;
 		return $item;
 	}
-	
-	
+
+
 	/**
 	 * Add new item to given index
 	 * @param int $index
@@ -131,7 +131,7 @@ class Menu {
 		$this->items[$index] = $item;
 		return $item;
 	}
-	
+
 	/**
 	 * Set item-class
 	 * @param string $name
@@ -144,11 +144,16 @@ class Menu {
 	public function addAttribute($name, $value) {
 		$this->attributes[$name] = $value;
 	}
-	
+
 	public function addClass($class) {
 		$this->addAttribute('class', $class);
 	}
-	
+
+	public function removeClass() {
+		unset($this->attributes['class']);
+		return $this;
+	}
+
 	protected function getAttributes($attributes) {
 		if(is_array($attributes) && count($attributes) > 0) {
 			$out = array();
@@ -160,12 +165,12 @@ class Menu {
 		}
 		return '';
 	}
-	
+
 	/**
 	 * Write html
 	 * @return string
 	 */
-	public function __toString() {		
+	public function __toString() {
 		$o = array();
 		if(count($this->items) > 0) {
 			$o[] = '<ul'.(($this->class) ? ' class="'.$this->class.'"' : '');
@@ -176,12 +181,12 @@ class Menu {
 			foreach($this->items as $item) {
 				foreach($item->getItems() as $key=>$i) {
 					/* Write html */
-					$o[] = sprintf('<li%1$s><a href="%2$s" title="%4$s"%5$s>%3$s</a>', 
-									$this->getAttributes($i['attributes']), 
-									$i['value'], 
-									$i['title'], 
-									htmlspecialchars($i['description']),
-									$this->getAttributes($i['linkAttributes']));
+					$o[] = sprintf('<li%1$s><a href="%2$s" title="%4$s"%5$s>%3$s</a>',
+						$this->getAttributes($i['attributes']),
+						$i['value'],
+						$i['title'],
+						htmlspecialchars($i['description']),
+						$this->getAttributes($i['linkAttributes']));
 					if(isset($i['content']) && is_array($i['content'])) {
 						foreach($i['content'] as $c) {
 							$o[]=$c->__toString();
@@ -194,9 +199,9 @@ class Menu {
 						$o[]=$this->content[$key]->__toString();
 					}
 					$o[]='</li>';
-				}		
+				}
 			}
-			
+
 			$o[] = '</ul>';
 			return join($o, '');
 		}
