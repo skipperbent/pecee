a390adaabc5455154c0af87548631cbf51348818
Simon Sessingø <simon.sessingoe@gmail.com>
2016-03-04 13:37:33 +0100

    [FEATURE] Optimisations
    - Removed type attributes on html elements, if Html5 is used.
    - Other minor optimisations.

--- src/UI/Html/HtmlLink.php
+++ src/UI/Html/HtmlLink.php
@@ -3,12 +3,14 @@ namespace Pecee\UI\Html;
 
 class HtmlLink extends Html {
 
-	public function __construct($href, $rel='stylesheet', $type='text/css') {
+	public function __construct($href, $rel = 'stylesheet', $type = null) {
 		parent::__construct('link');
+
 		$this->closingType = self::CLOSE_TYPE_SELF;
 		$this->addAttribute('href', $href);
 		$this->addAttribute('rel', $rel);
-		if(!is_null($type)) {
+
+		if($type !== null) {
 			$this->addAttribute('type', $type);
 		}
 	}
