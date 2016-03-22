f58302d601c6c6f7a89fba45704b5267feb90863
Simon Sessingø <simon.sessingoe@gmail.com>
2016-03-22 21:08:57 +0100

    [FEATURE] Switched to 100% composer autoloader.
    Note: it's a bit slower (from 20ms to > 35ms) unless you add XCache or OPCache, then it's actually less slow. It's all depending on how many dependencies you have installed though.

---
+++ bootstrap.php
@@ -0,0 +1,20 @@
+<?php
+
+$_ENV['framework_path'] = dirname(__DIR__);
+
+function pecee_autoloader($class) {
+    $file = explode('\\', $class);
+    $appname = array_shift($file);
+    $file = join(DIRECTORY_SEPARATOR, $file) . '.php';
+
+    $modules = \Pecee\Module::getInstance();
+    $module = $modules->get($appname);
+
+    if($module !== null) {
+        require_once $module . DIRECTORY_SEPARATOR . 'lib' . DIRECTORY_SEPARATOR . $file;
+    }
+}
+
+spl_autoload_register('pecee_autoloader');
+
+require_once 'config/helpers.php';
