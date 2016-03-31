33c030ad88a95d8507de9bbcbd6f21d48f134421
Simon Sessingø <simon.sessingoe@gmail.com>
2016-03-31 20:01:54 +0200

    [TASK] Fixed ValidateInputBoolean so it's not as strict.

--- src/Http/Input/Validation/ValidateInputBoolean.php
+++ src/Http/Input/Validation/ValidateInputBoolean.php
@@ -4,7 +4,7 @@ namespace Pecee\Http\Input\Validation;
 class ValidateInputBoolean extends ValidateInput {
 	public function validate() {
 		if($this->value) {
-			return ($this->value === true || $this->value === false);
+			return ($this->value == true || $this->value == false);
 		}
 		return true;
 	}
