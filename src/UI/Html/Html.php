0b801812b41beddce546062ef49b75d2b6bf211a
Simon Sessingø <simon.sessingoe@gmail.com>
2016-04-03 20:38:02 +0200

    [TASK] Added support adding to the same attribute twice in Html class.

--- src/UI/Html/Html.php
+++ src/UI/Html/Html.php
@@ -6,30 +6,30 @@ use Pecee\Widget\Widget;
 
 class Html {
 
-	protected $type;
-	protected $innerHtml;
-	protected $closingType;
-	protected $attributes;
+    protected $type;
+    protected $innerHtml;
+    protected $closingType;
+    protected $attributes;
 
-	const CLOSE_TYPE_SELF = 'self';
-	const CLOSE_TYPE_TAG = 'tag';
-	const CLOSE_TYPE_NONE = 'none';
+    const CLOSE_TYPE_SELF = 'self';
+    const CLOSE_TYPE_TAG = 'tag';
+    const CLOSE_TYPE_NONE = 'none';
 
-	public function __construct($type) {
-		$this->type = $type;
-		$this->attributes = array();
-		$this->closingType = self::CLOSE_TYPE_TAG;
-		$this->innerHtml = array();
-	}
+    public function __construct($type) {
+        $this->type = $type;
+        $this->attributes = array();
+        $this->closingType = self::CLOSE_TYPE_TAG;
+        $this->innerHtml = array();
+    }
 
-	/**
-	 * @param array $html
+    /**
+     * @param array $html
      * @return self
-	 */
-	public function setInnerHtml(array $html) {
-		$this->innerHtml = $html;
+     */
+    public function setInnerHtml(array $html) {
+        $this->innerHtml = $html;
         return $this;
-	}
+    }
 
 
     public function addInnerHtml($html) {
@@ -37,88 +37,98 @@ class Html {
         return $this;
     }
 
-	public function addWidget(Widget $widget) {
-		return $this->addInnerHtml($widget->__toString());
-	}
+    public function addWidget(Widget $widget) {
+        return $this->addInnerHtml($widget->__toString());
+    }
 
-	public function addMenu(Menu $menu) {
-		return $this->addInnerHtml($menu->__toString());
-	}
+    public function addMenu(Menu $menu) {
+        return $this->addInnerHtml($menu->__toString());
+    }
 
-	public function addItem(Html $htmlItem) {
-		return $this->addInnerHtml($htmlItem->__toString());
-	}
+    public function addItem(Html $htmlItem) {
+        return $this->addInnerHtml($htmlItem->__toString());
+    }
 
-	/**
-	 * Adds new attribute to the element.
-	 *
-	 * @param string $name
-	 * @param string $value
-	 * @return static
-	 */
-	public function addAttribute($name, $value='') {
-		$this->attributes[$name] = $value;
-		return $this;
-	}
+    /**
+     * Adds new attribute to the element.
+     *
+     * @param string $name
+     * @param string $value
+     * @return static
+     */
+    public function addAttribute($name, $value = '') {
+        if(!isset($this->attributes[$name])) {
+            $this->attributes[$name] = array($value);
+        } else {
+            foreach($this->attributes[$name] as $val) {
+                if($val === $value) {
+                    return $this;
+                }
+            }
+            $this->attributes[$name][] = $value;
+        }
 
-	public function attr($name, $value = '') {
-		return $this->addAttribute($name, $value);
-	}
+        return $this;
+    }
 
-	public function id($id) {
-		$this->attr('id', $id);
-		return $this;
-	}
+    public function attr($name, $value = '') {
+        return $this->addAttribute($name, $value);
+    }
 
-	public function style($css) {
-		$this->attr('style', $css);
-		return $this;
-	}
+    public function id($id) {
+        $this->attr('id', $id);
+        return $this;
+    }
 
-	protected function writeHtml() {
-		$output = '<'.$this->type;
-		foreach($this->attributes as $key=>$val) {
-			$output .= ' '.$key. (($val !== null || strtolower($key) === 'value') ? '="'.$val.'"' : '');
-		}
-		$output .= ($this->closingType === self::CLOSE_TYPE_SELF) ? '/>' : '>';
+    public function style($css) {
+        $this->attr('style', $css);
+        return $this;
+    }
+
+    protected function writeHtml() {
+        $output = '<'.$this->type;
+        foreach($this->attributes as $key =>  $val) {
+            $output .= ' '.$key. (($val !== null || strtolower($key) === 'value') ? '="' . join(' ', $val) . '"' : '');
+        }
+        $output .= ($this->closingType === self::CLOSE_TYPE_SELF) ? '/>' : '>';
         foreach($this->innerHtml as $html) {
             $output.=$html;
         }
-		$output .= (($this->closingType === self::CLOSE_TYPE_TAG) ? sprintf('</%s>',$this->type) : '');
-		return $output;
-	}
+        $output .= (($this->closingType === self::CLOSE_TYPE_TAG) ? sprintf('</%s>',$this->type) : '');
+        return $output;
+    }
 
-	/**
-	 * Add class
-	 * @param string $class
+    /**
+     * Add class
+     * @param string $class
      * @return static
-	 */
-	public function addClass($class) {
-		$this->addAttribute('class',$class);
-		return $this;
-	}
+     */
+    public function addClass($class) {
+        $this->addAttribute('class', $class);
+        return $this;
+    }
 
-	/**
-	 * @return string $closingType
-	 */
-	public function getClosingType() {
-		return $this->closingType;
-	}
+    /**
+     * @return string $closingType
+     */
+    public function getClosingType() {
+        return $this->closingType;
+    }
 
-	public function getType() {
-		return $this->type;
-	}
+    public function getType() {
+        return $this->type;
+    }
 
-	/**
-	 * @param string $closingType
-	 */
-	public function setClosingType($closingType) {
-		$this->closingType = $closingType;
-	}
+    /**
+     * @param string $closingType
+     */
+    public function setClosingType($closingType) {
+        $this->closingType = $closingType;
+    }
 
-	public function __toString() {
-		return $this->writeHtml();
-	}
+    public function __toString() {
+        return $this->writeHtml();
+    }
 
     public function getInnerHtml() {
         return $this->innerHtml;
