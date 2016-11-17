0a9a51f126b68c497b0f080f81b2bd42987b4942
Simon Sessingø <simon.sessingoe@gmail.com>
2016-11-17 18:00:30 +0100

    Bugfixes
    - Fixed widget method in Widget class not removing template if same as parent.
    - Optimised ModelFile.
    - Language related optimisations and bugfixes.
    - Changed load order for translation + locale.

--- src/Translation/Providers/DbTranslateProvider.php
+++ src/Translation/Providers/DbTranslateProvider.php
@@ -1,18 +1,60 @@
 <?php
 namespace Pecee\Translation\Providers;
 
+use Pecee\Model\Model;
 use Pecee\Model\ModelLanguage;
+use Pecee\SimpleRouter\RouterBase;
 
 class DbTranslateProvider implements ITranslationProvider {
 
-    protected $model;
+    /**
+     * @var Model
+     */
+    protected $translations;
+    protected $locale;
+    protected $defaultLocale;
+    protected $autoCreate = true;
 
     public function __construct() {
-        $this->model = new ModelLanguage();
+        $this->translations = new ModelLanguage();
+    }
+
+    public function getContext() {
+        $route = RouterBase::getInstance()->getLoadedRoute();
+
+        if($route && $route->getIdentifier()) {
+            return $route->getIdentifier();
+        }
+
+        return '';
     }
 
     public function lookup($key) {
-		return $this->model->lookup($key);
-	}
+        if($this->translations !== null && $this->translations->hasRows()) {
+
+            foreach($this->translations->getRows() as $lang) {
+                if(trim($lang->original) == trim($key)) {
+                    return $lang->translated;
+                }
+            }
+
+            if($this->autoCreate) {
+                // Save new key for translation
+                $lang = new ModelLanguage();
+                $lang->original = $key;
+                $lang->translated = $key;
+                $lang->context = $this->getContext();
+                $lang->save();
+            }
+        }
+
+        return $key;
+    }
+
+    public function load($locale, $defaultLocale) {
+        if($locale !== $defaultLocale) {
+            $this->translations = ModelLanguage::getByContext($this->getContext(), $locale);
+        }
+    }
 
 }
--- src/Translation/Providers/XmlTranslateProvider.php
+++ src/Translation/Providers/XmlTranslateProvider.php
@@ -5,58 +5,55 @@ use Pecee\Exceptions\TranslationException;
 
 class XmlTranslateProvider implements ITranslationProvider {
 
-	protected $xml;
-	protected $dir;
+    protected $locale;
+    protected $xml;
+    protected $dir;
 
-	public function __construct() {
-		$this->dir = env('XML_TRANSLATION_DIR', '../lang');
-		$this->setLanguageXml();
-	}
+    public function __construct() {
+        $this->dir = env('XML_TRANSLATION_DIR', '../lang');
+    }
 
-	public function lookup($key) {
-		if(!$this->dir) {
-			throw new TranslationException('XML language directory must be specified.');
-		}
+    public function lookup($key) {
+        $xml = new \SimpleXmlElement($this->xml);
+        $node = null;
 
-		$xml = new \SimpleXmlElement($this->xml);
-		$node = null;
+        if(strpos($key, '.') > -1) {
+            $children = explode('.', $key);
+            foreach($children as $i => $child) {
+                if($i === 0) {
+                    $node = (isset($xml->$child) ? $xml->$child : null);
+                } else {
+                    $node = (isset($node->$child) ? $node->$child : null);
+                }
+            }
+        } else {
+            $node = isset($xml->$key) ? $xml->$key : null;
+        }
 
-		if(strpos($key, '.') > -1) {
-			$children = explode('.', $key);
-			foreach($children as $i => $child) {
-				if($i === 0) {
-					$node = (isset($xml->$child) ? $xml->$child : null);
-				} else {
-					$node = (isset($node->$child) ? $node->$child : null);
-				}
-			}
-		} else {
-			$node = isset($xml->$key) ? $xml->$key : null;
-		}
+        if($node !== null) {
+            return $node;
+        }
 
-		if($node !== null) {
-			return $node;
-		}
+        throw new TranslationException(sprintf('Key "%s" does not exist for locale "%s"', $key, $this->locale));
+    }
 
-		throw new TranslationException(sprintf('Key "%s" does not exist for locale "%s"', $key, request()->locale->getLocale()));
-	}
+    public function load($locale, $defaultLocale) {
+        $this->locale = $locale;
+        $path = sprintf('%s/%s.xml', $this->dir, str_replace('-', '_', strtolower($locale)));
 
-	public function setLanguageXml() {
-		$path = sprintf('%s/%s.xml', $this->dir, str_replace('-', '_', strtolower(request()->locale->getLocale())));
+        if(!is_file($path)) {
+            throw new TranslationException(sprintf('Language file %s not found for locale %s', $path, $locale));
+        }
 
-		if(!is_file($path)) {
-			throw new TranslationException(sprintf('Language file %s not found for locale %s', $path, request()->locale->getLocale()));
-		}
+        $this->xml = file_get_contents($path, FILE_USE_INCLUDE_PATH);
+    }
 
-		$this->xml = file_get_contents($path, FILE_USE_INCLUDE_PATH);
-	}
+    public function setDirectory($dir) {
+        $this->dir = $dir;
+    }
 
-	public function setDirectory($dir) {
-		$this->dir = $dir;
-	}
-
-	public function getDir() {
-		return $this->dir;
-	}
+    public function getDir() {
+        return $this->dir;
+    }
 
 }
