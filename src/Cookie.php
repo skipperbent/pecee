d88c6b221ab662bd93b4c8c78009bb652c2bccb1
Simon Sessingø <simon.sessingoe@gmail.com>
2016-11-18 09:57:41 +0100

    Cookie optimisations

--- src/Cookie.php
+++ src/Cookie.php
@@ -1,19 +1,17 @@
 <?php
 namespace Pecee;
 
-use Carbon\Carbon;
-
 class Cookie {
 
-    public static function create($id, $value, Carbon $expireDate = null, $domain = null, $secure = null, $path = '/') {
-        $expireDate = ($expireDate === null) ? time() + 60 * 60 * 24 * 6004 : $expireDate->getTimestamp();
+    public static function create($id, $value, $expireTime = null, $domain = null, $secure = null, $path = '/') {
+        $expireTime = ($expireTime === null) ? time() + 60 * 60 * 24 * 6004 : $expireTime;
 
         if($domain === null) {
             $sub = explode('.', request()->getHost());
             $domain = (count($sub) > 2) ? request()->getHost() : '.' . request()->getHost();
         }
 
-        return setcookie($id, $value, (($expireDate > 0) ? $expireDate : null), $path, $domain, $secure);
+        return setcookie($id, $value, (($expireTime > 0) ? $expireTime : null), $path, $domain, $secure);
     }
 
     public static function get($id,$defaultValue = null) {
