373df1e438285534da3f1370670ae2b9d972506a
Simon Sessingø <simon.sessingoe@gmail.com>
2017-02-09 13:41:06 +0100

    Updated to simple-php-router version 3

--- src/Http/Middleware/BaseMiddleware.php
+++ src/Http/Middleware/BaseMiddleware.php
@@ -3,11 +3,10 @@ namespace Pecee\Http\Middleware;
 
 use Pecee\Base;
 use Pecee\Http\Request;
-use Pecee\SimpleRouter\Route\ILoadableRoute;
 
 abstract class BaseMiddleware extends Base implements IMiddleware
 {
 
-    abstract public function handle(Request $request, ILoadableRoute &$route);
+    abstract public function handle(Request $request);
 
 }
--- src/Http/Middleware/Middleware.php
+++ src/Http/Middleware/Middleware.php
@@ -2,11 +2,10 @@
 namespace Pecee\Http\Middleware;
 
 use Pecee\Http\Request;
-use Pecee\SimpleRouter\Route\ILoadableRoute;
 
 abstract class Middleware implements IMiddleware
 {
 
-    abstract public function handle(Request $request, ILoadableRoute &$route);
+    abstract public function handle(Request $request);
 
 }
