67e291683fed1f8b82d39ff5a9419dfc301e0eef
Simon Sessingo <simon.sessingoe@gmail.com>
2017-11-20 23:34:33 +0100

    Updated encryption to support php 7.2

--- src/Mcrypt.php
+++ src/Mcrypt.php
@@ -3,45 +3,58 @@ namespace Pecee;
 
 class Mcrypt {
 
-	public static function encrypt($dataInput, $key){
-		$td = mcrypt_module_open(MCRYPT_CAST_256, '', 'ecb', '');
-		$iv = mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-		mcrypt_generic_init($td, $key, $iv);
-		$encrypted_data = mcrypt_generic($td, $dataInput);
-		mcrypt_generic_deinit($td);
-		mcrypt_module_close($td);
-		$encoded_64 = base64_encode($encrypted_data);
-		return $encoded_64;
-	}
+    public static function encrypt($key, $data, $method = 'AES-256-CBC')
+    {
+        $key = substr(hash('sha256', $key, true), 0, 16);
 
-	public static function decrypt($encoded64, $key){
-		$td = mcrypt_module_open(MCRYPT_CAST_256, '', 'ecb', '');
-		$iv = mcrypt_create_iv (mcrypt_enc_get_iv_size($td), MCRYPT_RAND);
-		mcrypt_generic_init($td, $key, $iv);
-		$decrypted_data = mdecrypt_generic($td, base64_decode($encoded64));
-		mcrypt_generic_deinit($td);
-		mcrypt_module_close($td);
-		return $decrypted_data;
-	}
+        $isSourceStrong = false;
 
-	/**
-	 * Creates an random password, with a given length.
-	 * @param int $length
-	 * @return string
-	 * @throws \Exception
-	 */
-	public static function createRandomPassword($length) {
-		$chars = "ABCDEFGHIJKLMNOPQRSTUVXYXW023456789";
-		srand((double)microtime()*1000000);
-		$i = 0;
-		$pass = '' ;
-		while ($i <= $length) {
-			$num = rand() % 33;
-			$tmp = substr($chars, $num, 1);
-			$pass = $pass . $tmp;
-			$i++;
-		}
-		return $pass;
-	}
+        $iv = openssl_random_pseudo_bytes(16, $isSourceStrong);
+        if ($isSourceStrong === false || $iv === false) {
+            throw new \RuntimeException('IV generation failed');
+        }
+
+        $data = openssl_encrypt($data, $method, $key, 0, $iv);
+
+        return base64_encode($data . '|' . bin2hex($iv));
+    }
+
+    public static function decrypt($key, $data, $method = 'AES-256-CBC')
+    {
+        $key = substr(hash('sha256', $key, true), 0, 16);
+
+        list($data, $iv) = explode('|', base64_decode($data));
+
+        $binary = hex2bin($iv);
+        if ($binary === false) {
+            return false;
+        }
+
+        $data = openssl_decrypt($data, $method, $key, 0, $binary);
+
+        return $data;
+    }
+
+    /**
+     * Creates an random password, with a given length.
+     * @param int $length
+     * @return string
+     * @throws \Exception
+     */
+    public static function createRandomPassword($length)
+    {
+        $chars = 'ABCDEFGHIJKLMNOPQRSTUVXYXW023456789';
+        mt_srand((double)microtime() * 1000000);
+        $i = 0;
+        $pass = '';
+        while ($i <= $length) {
+            $num = mt_rand() % 33;
+            $tmp = substr($chars, $num, 1);
+            $pass .= $tmp;
+            $i++;
+        }
+
+        return $pass;
+    }
 
 }
