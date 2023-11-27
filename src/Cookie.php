c7191ff73d8a989502c4397c8873bd2725a25a01
Simon <simon.sessingoe@gmail.com>
2023-11-27 09:16:41 +0100

    Bugfixes

--- src/Cookie.php
+++ src/Cookie.php
@@ -11,20 +11,20 @@ class Cookie
      * Set cookie
      * @param string $id
      * @param string $value
-     * @param int $expireTime
+     * @param int|null $expireTime
      * @param string|null $domain
      * @param bool $secure
-     * @param string $path
+     * @param string|null $path
      * @return bool
      */
-    public static function create(string $id, string $value, ?Carbon $expireTime = null, ?string $domain = null, bool $secure = false, string $path = '/'): bool
+    public static function create(string $id, string $value, ?int $expireTime = null, ?string $domain = null, bool $secure = false, ?string $path = null): bool
     {
-        $expireTime = $expireTime ?? Carbon::now()->addYears(10);
+        $expireTime = $expireTime ?? Carbon::now()->addYears(10)->timestamp;
         $expireTime = ($expireTime > 0) ? $expireTime : null;
 
         $params = session_get_cookie_params();
 
-        return setcookie($id, $value, $expireTime->time, $path ?? $params['path'], $domain ?? $params['domain'], $secure);
+        return setcookie($id, $value, $expireTime, $path ?? $params['path'], $domain ?? $params['domain'], $secure);
     }
 
     public static function get(string $id, $defaultValue = null): ?string
