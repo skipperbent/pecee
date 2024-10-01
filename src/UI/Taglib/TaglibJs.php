b0c085da631daf48acb70020dabcc60fc52ee6c4
Simon <simon.sessingoe@gmail.com>
2024-10-01 21:51:08 +0200

    ...

--- src/UI/Taglib/TaglibJs.php
+++ src/UI/Taglib/TaglibJs.php
@@ -390,7 +390,7 @@ class TaglibJs extends Taglib
             $output = preg_replace('/";(}else\{|for|if]switch)/i', "\";\n$1", $output);
         }
 
-        $data = $attrs->data ?? 'null';
+        $data = $attrs->data ?? 'd';
         $elStartTag = $attrs->el ?? 'div';
         $elEndTag = $elStartTag;
         $as = $attrs->as ?? 'd';
